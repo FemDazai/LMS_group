b1af915eb08edd76e8c3bc4d70fa31281dad3d12
El-ItsMe <elnazteymurova001@gmail.com>
2022-12-13 22:59:38 +0400

    Create class WebClient, change scenario steps and add new methods

---
+++ LMSTestingProjectQAABaku/Models/AuthRequestModelApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LMSTestingProjectQAABaku.Models
+{
+    public class AuthRequestModelApi
+    {
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+    }
+}
---
+++ LMSTestingProjectQAABaku/WebClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using LMSTestingProjectQAABaku.Models;
+using System.Text.Json;
+
+namespace LMSTestingProjectQAABaku
+{
+    public class WebClient
+    {
+        public void SetRole(string token, int id, string role)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.NoContent;
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"https://piter-education.ru:7070/api/Users/{id}/role/{role}"),
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+            //int id = Convert.ToInt32(responseMessage.Content.ReadAsStringAsync().Result);
+
+        }
+        public string Auth(AuthRequestModelApi model)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.OK;
+            string json = JsonSerializer.Serialize<AuthRequestModelApi>(model);
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"https://piter-education.ru:7070/sign-in"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+            string token = responseMessage.Content.ReadAsStringAsync().Result;
+
+            return token;
+        }
+    }
+}
