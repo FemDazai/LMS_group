d0e4808224d4fef514711c890842866a74861f73
El-ItsMe <elnazteymurova001@gmail.com>
2022-12-18 14:17:43 +0400

    fix some changes in giveTeacherRole

--- LMSTestingProjectQAABaku/Pages/AuthPage.cs
+++ LMSTestingProjectQAABaku/Pages/AuthPage.cs
@@ -34,6 +34,5 @@ namespace LMSTestingProjectQAABaku.Pages
         {
             return TextBoxUserName.Text;
         }
-
     }
 }
---
+++ LMSTestingProjectQAABaku/Pages/TeacherMenuPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSTestingProjectQAABaku.Pages
+{
+    public class TeacherMenuPage : AbstractPage
+    {
+        public override void Open()
+        {
+        }
+
+        public IWebElement ButtonUserNameMenu => _driver.FindElement(By.XPath(@"//div[@class='drop-down-filter__wrapper']"));
+        public IWebElement ButtonSelectTeacher => _driver.FindElement(By.XPath(@"//li[text()='Преподаватель'][1]"));
+        public IWebElement ButtonAvatarName => _driver.FindElement(By.XPath(@"//span[@class='avatar-name transition-styles']"));
+        public IWebElement ButtonAvatarRole => _driver.FindElement(By.XPath(@"//div[@class='drop-down-filter  left']"));
+
+        public void GetClickButtonUserNameMenu()
+        {
+            ButtonUserNameMenu.Click();
+        }
+        public void GetClickButtonSelectTeacher()
+        {
+            ButtonSelectTeacher.Click();
+        }
+        public string GetTextButtonAvatarRole()
+        {
+            return ButtonAvatarRole.Text;
+        }
+        public string GetTextButtonAvatarName()
+        {
+            return ButtonAvatarName.Text;
+        }
+
+    }
+}
